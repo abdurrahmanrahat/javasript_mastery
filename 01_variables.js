e8b381e5d016fdc3c4a537db80e7e8c14cbe354b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-07-05 19:46:41 +0600

    variables

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234;
+let accountName = "Rahat";
+var accountEmail = "rahat@gmail.com";
+
+// accountId = 33; not allowed
+
+accountName = "Monju"; // allowed
+
+console.log(accountName);
+
+/*
+Prefer not to use var
+because of issue in block scope {} and functional scope
+*/
