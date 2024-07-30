c9ccb9b99cfec5541b336581045275d91fc842cf
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-07-30 18:10:12 +0600

    creating and exporting module

---
+++ 30_days_challenges/day13/creating_and_exporting_modules/export.mjs
@@ -0,0 +1,18 @@
+const addTwoNumbers = (number1, number2) => {
+    return number1 + number2;
+}
+
+const person = {
+    name: "rahat",
+    age: 21,
+
+    greeting: function() {
+        return `hello ${this.name}! how are you.`
+    }
+}
+
+export {
+    addTwoNumbers,
+    person
+};
+
---
+++ 30_days_challenges/day13/creating_and_exporting_modules/import.mjs
@@ -0,0 +1,6 @@
+import { addTwoNumbers, person } from "./export.mjs";
+
+console.log(addTwoNumbers(3, 4));
+
+console.log(person);
+console.log(person.greeting());
